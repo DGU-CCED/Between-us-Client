133b2960de4a6d19c0b6702c408203c0653ce222
bk123477 <bk123477@naver.com>
2022-11-24 20:06:29 +0900

    Blank Tab & List 구현

--- src/pages/auth/Home.js
+++ src/pages/auth/Home.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import ListView from '../../components/ListView';
 import TabList from '../../components/TabList';
 import "./Home.css"
 
 function Home() {
     return(
         <div class="home-container">
-            <div class="home_select"><TabList /></div>
+            <div class="home_select">
+                <TabList />
+                <ListView />
+            </div>
             
             <div class="test">Hi</div>
         </div>
