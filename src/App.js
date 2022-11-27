ab9a7f52825f48f9021bbd19b7115692dff50d82
222SeungHyun <93538221+222SeungHyun@users.noreply.github.com>
2022-11-27 18:15:35 +0900

    [Add] 칸반보드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Route, Routes} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/Header.js';
@@ -7,19 +7,21 @@ import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import Home from './pages/auth/Home';
 import HackathonDetail from './components/HackathonDetail';
+import Kanban from './components/kanban/index';
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div className="App">
         <Header />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route exact path='/home' element={<Home/>}/>
-          <Route exact path="/hackathon/detail/:id" element={<HackathonDetail/>}/>
-          <Route path='/login' element={<LoginPage />}/>
-          <Route path='/register' element={<RegisterPage />}/>
-        </Routes>       
+          <Route path='/' element={<Home />} />
+          <Route exact path='/home' element={<Home />} />
+          <Route exact path="/hackathon/detail/:id" element={<HackathonDetail />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/kanban' element={<Kanban />} />
+        </Routes>
       </div>
     );
   }
